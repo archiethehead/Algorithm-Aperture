f4325293ab08e12dfa4107240094a9392d14f64f
Archie Healy <archiethehead@gmail.com>
2026-01-20 01:55:07 +0000

    Sorting Test
    Fix: Implemented successful test case for bubble and selection sort.

--- algorithm_test.py
+++ algorithm_test.py
@@ -1,6 +1,7 @@
 import pytest
 import timeit
 from algorithms import dynamic_programming
+from algorithms import sorting
 
 class Test_fibonacci:
     @pytest.mark.parametrize("n, expected", [
@@ -10,5 +11,16 @@ class Test_fibonacci:
         (-3, 2), 
         (-4, -3)
     ])
-    def test_(self, n, expected):
+    def test_nth_fibonacci(self, n, expected):
         assert dynamic_programming.nth_fibonacci(n) == expected
+
+@pytest.mark.parametrize("data, ascending, expected", [
+        ([1, 2, 5, 6, 3, 2, -1], True, [-1, 1, 2, 2, 3, 5, 6]), 
+        ([1, 2, 5, 6, 3, 2, -1], False, [6, 5, 3, 2, 2, 1, -1])
+    ])
+class Test_sort:
+    def test_selection_sort(self, data, ascending, expected):
+        assert sorting.selection_sort(data.copy(), ascending) == expected
+
+    def test_bubble_sort(self, data, ascending, expected):
+        assert sorting.bubble_sort(data.copy(), ascending) == expected
