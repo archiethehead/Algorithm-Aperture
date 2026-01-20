eecbcdec0b9b207b18af130faba90d1dd80a92d9
Archie Healy <archiethehead@gmail.com>
2026-01-20 02:08:35 +0000

    Performance Test (fib and sort)
    Feature: Implemented performance tests for the selection/bubble sort, and nth_fibonacci.

--- algorithm_test.py
+++ algorithm_test.py
@@ -1,5 +1,5 @@
 import pytest
-import timeit
+import random
 from algorithms import dynamic_programming
 from algorithms import sorting
 
@@ -14,13 +14,28 @@ class Test_fibonacci:
     def test_nth_fibonacci(self, n, expected):
         assert dynamic_programming.nth_fibonacci(n) == expected
 
+fib_data = random.randint(0,1000)
+
+def test_mth_fibonacci_performance(benchmark):
+    benchmark(dynamic_programming.nth_fibonacci, fib_data)
+
 @pytest.mark.parametrize("data, ascending, expected", [
         ([1, 2, 5, 6, 3, 2, -1], True, [-1, 1, 2, 2, 3, 5, 6]), 
         ([1, 2, 5, 6, 3, 2, -1], False, [6, 5, 3, 2, 2, 1, -1])
     ])
+
 class Test_sort:
+
     def test_selection_sort(self, data, ascending, expected):
         assert sorting.selection_sort(data.copy(), ascending) == expected
 
     def test_bubble_sort(self, data, ascending, expected):
         assert sorting.bubble_sort(data.copy(), ascending) == expected
+
+sort_data = [random.randint(0, 1000) for x in range(1000)]
+
+def test_selection_sort_performance(benchmark):
+    benchmark(sorting.selection_sort, sort_data.copy(), True)
+
+def test_bubble_sort_performance(benchmark):
+    benchmark(sorting.bubble_sort, sort_data.copy(), True)
